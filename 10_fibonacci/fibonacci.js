b85d3629b87f3001059d6e0f4112eac4e4165dc1
NyroY64 <youcef.meradi09@gmail.com>
2023-09-29 01:52:23 +0200

    j'ai fini l'algorithme fibonacci j'ai valide tous les test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,18 +1,22 @@
 const fibonacci = function(number) {
-var sum=0;
-    
-    for (let i = 0; i < number; i++) {
-        
-        for (let j = 0; j < i; j++) {
-            sum +=j;
-            
-        }
 
-        
+    if(number <0 )return "OOPS"; 
+
+    let current =1;
+    let last =0;
+    let tmp;
+    for (let i = 0; i < (number-1); i++) {
+        tmp = current;
+        current = current + last;
+        last = tmp;
+    
     }
+    return current;
    
    
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
